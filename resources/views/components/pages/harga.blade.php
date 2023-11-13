e79f223bece626ab1c68b350e8c282958529c0d3
Muhammad Badrun <bbadrunn@gmail.com>
2023-11-13 19:44:33 +0700

    update

--- resources/views/components/pages/harga.blade.php
+++ resources/views/components/pages/harga.blade.php
@@ -59,7 +59,7 @@
                         <span class="text-5xl font-semibold tracking-tight">@currency($finalPrice)</span>
                      </div>
                      <a href="https://api.whatsapp.com/send?phone=6285721557240&text=Halo%20Duos%20Dev,%20Saya%20Mau%20order%20jasa%20pembuatan%20web%20{{$item->service->name}}%20dengan%20{{$item->packet->name}}.%20Mohon%20infonya%20lebih%20lanjut." target="_blank" class="mb-2 text-slate-100 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Info Lebih Lanjut</a>
-                     <ul role="list" class="space-y-5 my-7 list-none">
+                     <ul role="list" class="space-y-5 my-7 list-none" style="padding: 0 !important">
                       
                       @foreach($item->features as $feature)
                         
